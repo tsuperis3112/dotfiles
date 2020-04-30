c40d0c17c613dce5a0ad40ffb738162af5073b65
tsuperis <tsuperis@gmail.com>
2020-04-30 23:33:14 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+eflg=0
+
+for file in `\ls $SCRIPT_DIR | grep -v setup.sh`; do
+	src_path=$SCRIPT_DIR/$file
+	dst_path=$HOME/.$file
+
+	if [ ! -e "$dst_path" ]; then
+		ln -s $src_path $dst_path
+	elif [ ! -L "$dst_path" ] || [ "$src_path" != `\readlink $dst_path` ]; then
+		echo "Error: $file is exist" >&2
+		eflg=1
+	fi
+done
+
+exit $eflg
