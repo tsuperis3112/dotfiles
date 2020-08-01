58e96e449d130bbb80aec32e63f5dba9297705cc
tsuperis <tsuperis@gmail.com>
2020-08-01 19:01:21 +0900

    copy only ssh config

--- setup.sh
+++ setup.sh
@@ -4,12 +4,14 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 eflg=0
 
 for file in `\ls $SCRIPT_DIR | grep -v setup.sh`; do
+        if [ $file = "ssh" ]; then
+		file="${file}/config"
+	fi
+
 	src_path=$SCRIPT_DIR/$file
 	dst_path=$HOME/.$file
 
-        if [ $file = "ssh" ]; then
-                ln -s $src_path/config $dst_path/config
-	elif [ ! -e "$dst_path" ]; then
+	if [ ! -e "$dst_path" ]; then
 		ln -s $src_path $dst_path
 	elif [ ! -L "$dst_path" ] || [ "$src_path" != `\readlink $dst_path` ]; then
 		echo "Error: $file is exist" >&2
